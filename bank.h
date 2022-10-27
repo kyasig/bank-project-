290b4887be080d4145e653822c23578c99892ead
kyasig <92475855+kyasig@users.noreply.github.com>
2022-10-26 20:36:56 -0700

    test1
    testing some shit out

--- bank.h
+++ bank.h
@@ -1,9 +1,6 @@
 #include <iostream>
 using namespace std;
-
 class bank {
-
+    public:
+        void test(){cout << "testing";}
 };
-
-
-
--- bankProj.cpp
+++ bankProj.cpp
@@ -4,5 +4,7 @@
 using namespace std;
 int main()
 {
+	bank* sex;
+	sex->test();
 	return 0;
 }
