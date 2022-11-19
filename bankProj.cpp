ade09ca73dcfbc8c388a4b121e4390e7807b6617
kyasig <92475855+kyasig@users.noreply.github.com>
2022-11-18 23:01:48 -0800

    updated subaccount.h
    added validinput function
    menu for subaccount mostly done
    need to work on overriding it

--- bankProj.cpp
+++ bankProj.cpp
@@ -2,15 +2,20 @@
 #include <iostream>
 #include "bank.h"
 #include "subaccount.h"
+#include <vector>
 using namespace std;
 int main()
 {
 	subAccount sub1;
-    checkingAccount sub2;
-
-	sub1.deposit(69);
+    checkingAccount *sub2;
 	//sub2.deposit(440);
 	//sub2.withdraw(20);
-	
-	sub1.menu();
+	//sub1.menu();
+	vector<subAccount *> subvec;
+	//subvec.push_back(new checkingAccount());
+	subvec.push_back(new subAccount());
+	subvec.push_back(new checkingAccount());
+	//subvec.at(0)->menu();
+	subvec.at(1)->menu();
+	//cout << sub1.getBalance();
 }
--- subaccount.h
+++ subaccount.h
@@ -1,18 +1,20 @@
 #ifndef subaccount_H
 #define subaccount_H
+#include <iostream>
 #include <string>
+using namespace std;
 static int uniqueNumSav = 1000;
 static int uniqueNumChek = 6000;
 class subAccount{
     protected:
-        int balance;
+        unsigned int balance;
         string num; 
     public:
-        virtual void deposit(int num){this->balance += num;};
-        void withdraw(int num){this->balance -= num;}
+        void deposit();
+        void withdraw(unsigned int max);
         string getNum(){return this->num;}
         int getBalance(){return this->balance;}
-        virtual void menu();
+        virtual void menu() const;
         subAccount();
 };
 class checkingAccount : public subAccount{
@@ -23,31 +25,69 @@ class checkingAccount : public subAccount{
     public:
         int setMax();
         void toggleLock();
+        void menu() const override{
+            cout << "sex";
+        }
         checkingAccount();
 };
 
-subAccount :: subAccount(){
-    this->num = "SAV" + std::to_string(uniqueNumSav++);
-}
-checkingAccount :: checkingAccount(){
-    this->num = "CHK" + std::to_string(uniqueNumChek++);
-    this->balance = 0;
-}
-
-void subAccount :: menu(){
-    std::cout <<"eligable services for " << this->getNum() << endl << "--D Deposit" << endl << "--W withdraw" << endl << "--X exit";
-    string input;
-    while (true){
-        cin >> input;
-        switch (input){
-            case "d": this->deposit(1);
-            break;
-            case "w": this->withdraw(1);
-            break;
-            case "x": return;
-            break;
+int validInput(string input){
+    cin >> input;
+    while(true){
+        try{
+            return stoi(input);
+        }catch(invalid_argument){
+            cout <<"enter a damn integer: ";
+            cin >> input;
         }
     }
 }
 
+subAccount :: subAccount(){
+    this->num = "SAV" + to_string(uniqueNumSav++);
+}
+
+checkingAccount :: checkingAccount(){
+    this->num = "CHK" + to_string(uniqueNumChek++);
+    string maxstr;
+    cin >> maxstr;
+    this->maxCapacity = validInput(maxstr);
+    this->balance = 0;
+}
+
+void subAccount::deposit(){
+    cout << "enter deposit amount ";
+    string input;
+    this->balance += validInput(input);
+}
+
+void subAccount :: withdraw(unsigned int max){
+    cout <<"how much do you wanna withdraw ";
+    string input;
+    int sub = validInput(input);
+    if(max >= sub){
+        this->balance -= sub;
+    }else{
+        cout <<"you dont have that much money to withdraw ";
+        }
+}
+
+void subAccount :: menu(){
+    while (true){
+        cout <<"eligable services for " << this->getNum() << endl << "--D Deposit" << endl << "--W withdraw" << endl << "--X exit";
+        char input;
+        cout <<endl<<"enter response ";
+        cin >> input;
+        if(input == 'd'){
+            this->deposit();
+        }
+        else if(input == 'w'){
+            this->withdraw(this->balance);
+        }
+        else if(input == 'x'){break;}
+        else{cout << "enter one of the options bruh" << endl;}
+        }
+    }
+
+  
 #endif
