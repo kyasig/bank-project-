cbace026ca3399d29f7c9d6fd974a885240f8a4d
kyasig <92475855+kyasig@users.noreply.github.com>
2022-11-21 23:38:35 -0800

    subaccount.h is basically 100% complete
    yay (:

--- bankProj.cpp
+++ bankProj.cpp
@@ -6,16 +6,11 @@
 using namespace std;
 int main()
 {
-	subAccount sub1;
-    checkingAccount *sub2;
-	//sub2.deposit(440);
-	//sub2.withdraw(20);
-	//sub1.menu();
-	vector<subAccount *> subvec;
-	//subvec.push_back(new checkingAccount());
-	subvec.push_back(new subAccount());
-	subvec.push_back(new checkingAccount());
-	//subvec.at(0)->menu();
-	subvec.at(1)->menu();
-	//cout << sub1.getBalance();
+	vector<subAccount*> s;
+	//subvec.push_back(new checking((Account());
+	s.push_back(new checkingAccount());
+	subAccount cum;
+	//cum.menu();
+    s.at(0)->menu();
+	return 0;
 }
--- subaccount.h
+++ subaccount.h
@@ -10,24 +10,25 @@ class subAccount{
         unsigned int balance;
         string num; 
     public:
-        void deposit();
-        void withdraw(unsigned int max);
+        virtual void deposit(int amount){this->balance += amount;};
+        virtual void withdraw(int amount){this->balance -= amount;};
         string getNum(){return this->num;}
-        int getBalance(){return this->balance;}
-        virtual void menu() const;
+        void printBalance(){cout << "current balance is " <<  this->balance << endl;}
+        void genericMenu(char input);
+        virtual void menu();
         subAccount();
 };
 class checkingAccount : public subAccount{
     private:
         int maxCapacity;
         bool locked;
-        void deposit();
     public:
-        int setMax();
+        void deposit(int amount);
+        void withdraw(int amount) override;
+        void setMax(int amount){this->maxCapacity = amount;};
         void toggleLock();
-        void menu() const override{
-            cout << "sex";
-        }
+        void menu()override;
+        void printt(){cout << "cum"<<endl;}
         checkingAccount();
 };
 
@@ -42,52 +43,87 @@ int validInput(string input){
         }
     }
 }
-
+void subAccount::genericMenu(char input){
+     if(tolower(input) == 'd'){
+        cout << "enter deposit amount ";
+        string input;
+        this->deposit(validInput(input));
+      }
+      else if(tolower(input) == 'w'){
+         cout <<"how much do you wanna withdraw ";
+         string input;
+         this ->withdraw(validInput(input));
+    }
+    else{cout << "enter one of the options bruh" << endl;}
+    this->printBalance();
+}
+/// @subaccount functions /////////////////////////////////////////////////////////////////////////////////////////////////////////
 subAccount :: subAccount(){
     this->num = "SAV" + to_string(uniqueNumSav++);
-}
-
-checkingAccount :: checkingAccount(){
-    this->num = "CHK" + to_string(uniqueNumChek++);
-    string maxstr;
-    cin >> maxstr;
-    this->maxCapacity = validInput(maxstr);
-    this->balance = 0;
-}
-
-void subAccount::deposit(){
-    cout << "enter deposit amount ";
-    string input;
-    this->balance += validInput(input);
-}
-
-void subAccount :: withdraw(unsigned int max){
-    cout <<"how much do you wanna withdraw ";
-    string input;
-    int sub = validInput(input);
-    if(max >= sub){
-        this->balance -= sub;
-    }else{
-        cout <<"you dont have that much money to withdraw ";
-        }
 }
 
 void subAccount :: menu(){
     while (true){
         cout <<"eligable services for " << this->getNum() << endl << "--D Deposit" << endl << "--W withdraw" << endl << "--X exit";
+        cout <<"enter response: ";
         char input;
-        cout <<endl<<"enter response ";
         cin >> input;
-        if(input == 'd'){
-            this->deposit();
-        }
-        else if(input == 'w'){
-            this->withdraw(this->balance);
-        }
-        else if(input == 'x'){break;}
-        else{cout << "enter one of the options bruh" << endl;}
+        if(tolower(input) == 'x'){
+            return;
+        }else{
+         genericMenu(input);
         }
     }
+}
+/////////////////////checking account functions//////////////////////////////////////////////////////////////////////
+checkingAccount :: checkingAccount(){
+    this->num = "CHK" + to_string(uniqueNumChek++);
+    this->balance = 100;
+    this->locked = false;
+    this->maxCapacity = 10;
+}
+void checkingAccount::withdraw(int amount ){
+    if(this->locked){cout << "account is locked" << endl; return;}
+    if(amount > this->maxCapacity){
+        cout << "you are trying to withdraw more than the max";
+        return;
+    }
+    subAccount :: withdraw(amount);
+}
+void checkingAccount :: deposit(int amount ){
+    if(this->locked){cout << "account is locked" << endl; return;}
+    subAccount :: deposit(amount);
+}
 
+
+void checkingAccount::toggleLock(){
+    string lockStatus;
+    this->locked = !this->locked;
+    this->locked? lockStatus = "locked" : lockStatus = "unlocked";
+    cout << "this account has been " << lockStatus << endl;
+}
+void checkingAccount :: menu(){
+    while(true){
+        cout <<"eligable services for " << this->getNum() << endl << "--D Deposit" << endl << "--W withdraw" << endl
+    << "--C max capacity" << endl << "--T toggle account lock status" << endl << "--X exit";
+        cout << endl <<"enter response ";
+        char input;
+        cin >> input; 
+        if(input == 'x'){
+        return;
+        }
+        else if(tolower(input) == 'c'){
+            cout << "enter max ";
+            string inp;
+            setMax(validInput(inp));
+        }
+        else if(tolower(input) == 't'){
+            toggleLock();
+        }
+        else{
+            subAccount::genericMenu(input);
+        }
+    }
+}
   
 #endif
